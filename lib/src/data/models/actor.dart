aa1724cbbd367d2a8aabe3a82d9c6d0ecbd687d4
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-06-03 18:18:50 -0400

    feat: add biography

--- lib/src/data/models/actor.dart
+++ lib/src/data/models/actor.dart
@@ -2,16 +2,19 @@ class Actor {
   final String name;
   final int id;
   final String profilePath;
+  final String biography;
 
   Actor({
     required this.name,
     required this.id,
     required this.profilePath,
+    required this.biography,
   });
 
   factory Actor.fromJson(json) => Actor(
         name: json['name'],
         id: json['id'],
         profilePath: json['profile_path'],
+        biography: json['biography'],
       );
 }
