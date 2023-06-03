0a6cf0e4941f7e52635a13a59dba10514765b005
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-06-03 18:36:03 -0400

    feat: maxLifes

--- lib/src/core/constants.dart
+++ lib/src/core/constants.dart
@@ -1,3 +1,4 @@
 const String apiKey = '4ccb9728518c7816c9c3ea08c0c1c296';
 const String imagesBaseUrl = 'https://image.tmdb.org/t/p/w500';
 const int maxActors = 5000;
+const int maxLifes = 5;
--- lib/src/ui/widgets/game_lifes.dart
+++ lib/src/ui/widgets/game_lifes.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:cineando/src/core/constants.dart';
+
 class GameLifes extends StatelessWidget {
   const GameLifes({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const maxLifes = 3;
     const lifes = 2;
     final lifesIcons = List.generate(
       maxLifes,
